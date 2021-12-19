0f7b872b6dcf710a48cf61fa133cc164f0f649b1
Lucian Cardoso <luciancardoso20@gmail.com>
2021-12-18 22:50:27 -0400

    Add files via upload

---
+++ controle/desafio.dart
@@ -0,0 +1,15 @@
+
+main(){
+
+  // aqui usando valor NUMERICO
+  
+  // var valor = '#';
+  // for(int i = 0; i < 6; i++){
+  //   print(valor);
+  //   valor += '#';
+  // }
+
+  for(var valor = '#'; valor != '#######'; valor += '#'){
+    print(valor);
+  }
+}
